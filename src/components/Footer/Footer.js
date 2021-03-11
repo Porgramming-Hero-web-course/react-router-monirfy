8972d9d648da3f4f8ea8ecaeebfdccce9aeb5641
Monir H <monirhfy@gmail.com>
2021-03-11 17:53:01 +0600

    Footer add row

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
     <footer className='py-5 bg-light text-center'>
       <Container>
-        <h3>Fantasy Football</h3>
-        <p className='lead mb-0'>
-          {' '}
-          This template made with <span className='text-danger'>❤</span> by Monir Hossain
-        </p>
+        <Row>
+          <Col lg={12}>
+            <h3>Fantasy Football</h3>
+            <p className='lead mb-0'>
+              This template made with <span className='text-danger'>❤</span> by Monir Hossain
+            </p>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
