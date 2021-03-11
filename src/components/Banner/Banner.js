28f6aec1b8d1f3e2ca6378d26c12b83eaff59944
Monir H <monirhfy@gmail.com>
2021-03-11 18:09:57 +0600

    banner problem test

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
   return (
     <Container fluid className='banner'>
       <Row>
-        <Col>
+        <Col lg={12}>
           <FontAwesomeIcon icon={faFutbol} size='6x' className='text-white' />
           <h1 className='banner-title'>Fantacy Football</h1>
         </Col>
